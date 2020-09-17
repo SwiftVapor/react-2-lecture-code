ce2f41eb1c5c8f98a968b672a673526b6abd10b1
SwiftVapor <jonathongthayne@gmail.com>
2020-09-17 14:45:26 -0600

    completed lecture code

--- src/App.js
+++ src/App.js
@@ -10,13 +10,50 @@ class App extends Component {
       username: 'Matt',
       todoList: ['Code the things', 'Eat', 'Sleep']
     }
+    //BIND THIS BOI
+    this.changeUsername = this.changeUsername.bind(this);
   }
-
+// functions will need to be bound to them selves to be used
+  changeUsername(){
+    this.setState({username: 'Eric'})
+  };
+  // public Classfield Syntax
+  // arrow functions can get around needing to bind the function to itself in the constructor 
+  //function
+  properContext = () =>{
+    this.setState({username: 'Aaron'})
+  }
   render(){
+    // const mappedTodos = this.state.todoList.map((todo, i)=>{
+    //   return (
+    //     <h2 key={i}>{todo}</h2>
+    //   )
+    // })
+
+    const mappedTodos = this.state.todoList.map ((todo, i ) =>{
+      return(
+      <ListDisplay key = {i} todo={todo}/>
+    )
+    })
     return (
       <div className="App">
-        <Header />
+        <Header username = {this.state.username} 
+        greeting = 'Hello World' 
+        usernameFn= {this.changeUsername}
+        secondUsernameFn= {this.properContext}/>
         <h1>My Todo List</h1>
+        {mappedTodos}
+        {/* <ListDisplay todo= {this.state.todoList[0]}/>
+        <ListDisplay todo= {this.state.todoList[1]}/>
+        <ListDisplay todo= {this.state.todoList[2]}/> */}
+        
+        {/* alternative to mapped todos variable */}
+        {this.state.todoList.map((todo, i )=>{
+          return (
+            <ListDisplay key ={i} todo ={todo}/>
+          )
+        })}
+
       </div>
     )
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
-const Header = () => {
+// functional components need to be passed props as an argument ot receive them, so you ned to place a 
+//props parameter to make props work properly
+const Header = props => {
+   console.log (props) 
+    
+    
     return (
-        <header className='main-header'></header>
+        <header className='main-header'>
+            <h1>
+                {props.greeting} {props.username}
+            </h1>
+            <button onClick={props.usernameFn}>Change Username</button>
+            <button onClick={props.secondUsernameFn}>Change Username again</button>
+        </header>
     )
 }
 
